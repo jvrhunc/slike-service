a64baadbabdb118d27a2c84f964a6d7345ca1b81
Jernej Vrhunc <jv4739@student.uni-lj.si>
2021-01-04 11:54:56 +0100

    Delo na konfiguraciji (Consul), Health Checkih, Discovery clientu.

--- src/main/java/si/fri/rso/slike/SlikeServiceApplication.java
+++ src/main/java/si/fri/rso/slike/SlikeServiceApplication.java
@@ -2,10 +2,12 @@ package si.fri.rso.slike;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories("si.fri.rso.slike.repositories")
+@EnableDiscoveryClient
 public class SlikeServiceApplication {
 
 	public static void main(String[] args) {
