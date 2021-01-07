18b5cfcb02bd56396ba3008e149afcb5f4947d3c
Jernej Vrhunc <jv4739@student.uni-lj.si>
2021-01-07 22:49:53 +0100

    Init data configured.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO slika (created, recept_id, url)
+        VALUES ('2020-12-1', 1, 'https://s3.eu-central-1.amazonaws.com/rso-slike-service-s3/1610054723412-tiramisu.jpg');
+INSERT INTO slika (created, recept_id, url)
+        VALUES ('2021-4-1', 2, 'https://s3.eu-central-1.amazonaws.com/rso-slike-service-s3/1610054706056-wellington_pecenka.jpg');
+INSERT INTO slika (created, recept_id, url)
+        VALUES ('2020-1-30', 3, 'https://s3.eu-central-1.amazonaws.com/rso-slike-service-s3/1610054669942-jota.jpg');
